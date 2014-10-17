92ab2201ad9b8bcd3f245e31ee2ce7a13ea1b3e7
meyerp <pierremeyer@gmx.fr>
2014-10-17 12:22:33 +0200

    Add footer and some links.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,7 @@ class HomeController < ApplicationController
 
   def terms
   end
+
+  def privacy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'contact/form'
+
 root 'home#index'
 
 get 'home/index'
 get 'home/terms'
+get 'home/privacy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
